49fa852faec0a9cb1212b00dffa11f2ee15e1ab2
Namrata555 <namratakuila555@gmail.com>
2026-04-18 21:37:41 +0530

    UC3 implemented - display name with default value

---
+++ HelloApp.java
@@ -0,0 +1,15 @@
+class HelloApp {
+    public static void main(String[] args) {
+
+        // Default name
+        String name = "World";
+
+        // Check if user provided argument
+        if (args.length > 0) {
+            name = args[0];
+        }
+
+        // Print greeting
+        System.out.println("Hello, " + name + "!");
+    }
+}
